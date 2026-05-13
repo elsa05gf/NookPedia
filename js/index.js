167cbb6cd5c0f3ecdfdf5d936d5ca7b2312d399b
Aspo0 <162562344+Aspo0@users.noreply.github.com>
2026-05-13 16:32:24 +0200

    Slider sin Bootstrap por que no me sale
    Lo he intentado, de verdad que si

--- js/index.js
+++ js/index.js
@@ -9,3 +9,76 @@ document.querySelectorAll('nav a[href^="#"]').forEach(anchor => {
 });
 
 const API_KEY = "955e9378-ac80-4d2d-9f7f-09003656bb3c";
+
+const track = document.getElementById("track");
+
+// PETICIÓN A LA API
+
+async function loadVillagers(){
+
+    const response = await fetch("https://api.nookipedia.com/villagers",{
+
+        headers:{
+            "X-API-KEY": API_KEY,
+            "Accept-Version":"1.0.0"
+        }
+
+    });
+
+    const data = await response.json();
+
+    console.log(data);
+
+    // SOLO ALGUNOS PERSONAJES
+    const villagers = data.slice(0,15);
+
+    villagers.forEach(villager => {
+
+        // CREAR DIV
+        const card = document.createElement("div");
+
+        card.classList.add("character-card");
+
+        // CREAR IMG
+        const img = document.createElement("img");
+
+        img.src = villager.image_url;
+
+        img.alt = villager.name;
+
+        // AÑADIR IMG AL DIV
+        card.appendChild(img);
+
+        // AÑADIR DIV AL TRACK
+        track.appendChild(card);
+
+    });
+
+}
+
+loadVillagers();
+
+const next = document.getElementById("next");
+const prev = document.getElementById("prev");
+
+let currentPosition = 0;
+
+next.addEventListener("click", () => {
+
+    currentPosition -= 300;
+
+    track.style.transform = `translateX(${currentPosition}px)`;
+
+});
+
+prev.addEventListener("click", () => {
+
+    currentPosition += 300;
+
+    if(currentPosition > 0){
+        currentPosition = 0;
+    }
+
+    track.style.transform = `translateX(${currentPosition}px)`;
+
+});
